b794b4f60b8e286fd829c70cb567504cc074f929
DIDULA <didulakodytuakku18@gmail.com>
2020-08-15 15:36:52 +0530

    Didula Commit

---
+++ app/src/main/java/com/example/home/BMI_OUTPUTSS.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BMI_OUTPUTSS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_m_i__o_u_t_p_u_t_s_s);
+    }
+}
---
+++ app/src/main/java/com/example/home/MUSCLEGROUPSS.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MUSCLEGROUPSS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_u_s_c_l_e_g_r_o_u_p_s_s);
+    }
+}
